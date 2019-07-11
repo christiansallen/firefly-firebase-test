2d482bfaa503526adbc1bbca9bba00feb1753520
Christian <christiansallen@gmail.com>
2019-07-10 18:20:48 -0600

    facebook/google/email auth setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import firebase from 'firebase';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-function App() {
-  return (
+firebase.initializeApp({
+  apiKey: "AIzaSyACMmtTos8CBls03uYsnOxUfP3jYFqSML0",
+  authDomain: "firefly-firebase-test.firebaseapp.com"
+})
+
+class App extends Component{
+  constructor(props){
+    super();
+    this.state={
+      signedIn: false
+    }
+  }
+
+  
+
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  signOut = () => {
+    firebase.auth().signOut();
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ signedIn: !!user })
+      console.log("user", user)
+    })
+  }
+
+render(){
+ return (
     <div className="App">
-      <h1>App home page</h1>
+      {this.state.signedIn ? <div>Signed In!<button onClick={this.signOut}>Sign Out</button></div> : <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />}
     </div>
   );
 }
 
+}
 export default App;
